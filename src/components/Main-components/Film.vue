eaf0580e379ea25ed5f73acdf04b0db2e140f9f5
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-20 16:17:44 +0100

    Milestone_1

---
+++ src/components/Main-components/Film.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="film">
+        <h4>{{ogg.title}}</h4>
+        <h4>{{ogg.original_title}}</h4>
+        <h4>{{ogg.original_language}}</h4>
+        <h4>{{ogg.popularity}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Film",
+    props: {
+        ogg: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,51 @@
 <template>
   <div id="main">
-      <input type="text">
-      <button>Cliccami</button>
+      <input type="text" v-model="searchBar">
+      <button @click="search()">Cliccami</button>
+      <!-- <p>{{risultato}}</p> -->
+      <Film v-for="(film,index) in risultato" :key="index"
+        :ogg = "film"
+        />
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Film from "../components/Main-components/Film.vue"
 export default {
     name: 'Main',
+    components: {
+        Film,
+    },
+    data() {
+        return {
+            searchBar: '',
+            risultato:[],
+        }
+    },
+    methods: {
+        search() {
+            axios.get("https://api.themoviedb.org/3/search/movie/", {
+                params: {
+                    api_key:"609576eb1e4d09dcf2d6888012fbf56c",
+                    query:this.searchBar
+                }
+            })
+            .then(response => {
+                this.risultato = response.data.results
+                // console.log(response.data.results)
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+        }
+    }
 }
+
+
+
 </script>
 
 <style>
