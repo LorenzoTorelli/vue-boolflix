7e14db5eb9621c0ef31e1a57174f3aeed5b1229f
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-20 15:50:36 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="main">
+      <input type="text">
+      <button>Cliccami</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+}
+</script>
+
+<style>
+
+</style>
